0dee85f672ed775beb6528dc42cec05ae6ee2b17
KostyantinSolod <solodkosta@gmail.com>
2023-12-04 21:50:22 +0200

    Авторизація
    Добавив регістрацію, яка записує дані в базу даних, і також добавив в авторизацію перевірку, чи є такий аккаунт, чи ні

--- Ukrainian-greenhouse/Ukrainian-greenhouse/MainWindow.xaml.cs
+++ Ukrainian-greenhouse/Ukrainian-greenhouse/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Npgsql;
 
 namespace Ukrainian_greenhouse
 {
@@ -20,9 +21,13 @@ namespace Ukrainian_greenhouse
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string connectionString = "Host = localhost;Username=postgres;Password=2002;Database=Ukrainian-greenhouse";
+        private NpgsqlConnection connection;
         public MainWindow()
         {
             InitializeComponent();
+            connection = new NpgsqlConnection(connectionString);
+
         }
 
         private void Registration_Click(object sender, RoutedEventArgs e)
@@ -31,5 +36,60 @@ namespace Ukrainian_greenhouse
             registration.Show();
             this.Close();
         }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (Login.Text != "" && Password.Password != "")
+            {
+                string login = Login.Text;
+                string password = Password.Password;
+
+                // Виклик методу для перевірки логіну та паролю
+                bool isValidUser = ValidateUser(login, password);
+
+                if (isValidUser)
+                {
+                    // Якщо користувач з таким логіном та паролем знайдений
+                    // Тут ви можете виконати дії, які потрібно виконати при успішному вході
+                    MessageBox.Show("Успішний вхід!");
+                }
+                else
+                {
+                    // Повідомлення про невірний логін або пароль
+                    MessageBox.Show("Невірний логін або пароль!");
+                }
+            }
+            else
+            {
+                // Повідомлення про те, що поля логіну та пароля повинні бути заповненими
+                MessageBox.Show("Будь ласка, введіть логін та пароль!");
+            }
+        }
+
+        private bool ValidateUser(string login, string password)
+        {
+            bool isValid = false;
+            try
+            {
+                connection.Open();
+                string query = "SELECT COUNT(*) FROM users WHERE login = @Login AND password = @Password";
+                using (NpgsqlCommand cmd = new NpgsqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("Login", login);
+                    cmd.Parameters.AddWithValue("Password", password);
+                    isValid = (long)cmd.ExecuteScalar() > 0;
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return isValid;
+        }
+
     }
 }
--- Ukrainian-greenhouse/Ukrainian-greenhouse/Registration.xaml.cs
+++ Ukrainian-greenhouse/Ukrainian-greenhouse/Registration.xaml.cs
@@ -11,17 +11,20 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Npgsql;
+
 
 namespace Ukrainian_greenhouse
 {
-    /// <summary>
-    /// Логика взаимодействия для Registration.xaml
-    /// </summary>
     public partial class Registration : Window
     {
+        public string connectionString = "Host = localhost;Username=postgres;Password=2002;Database=Ukrainian-greenhouse";
+        private NpgsqlConnection connection;
         public Registration()
         {
             InitializeComponent();
+            connection = new NpgsqlConnection(connectionString);
+
         }
 
         private void Login_Click(object sender, RoutedEventArgs e)
@@ -33,15 +36,123 @@ namespace Ukrainian_greenhouse
 
         private void Registration_Click(object sender, RoutedEventArgs e)
         {
-            
-            if (PasswordText.Password == SecondPasswordText.Password)
+            emailText.Text = "";
+            loginText.Text = "";
+            RepeatpasswordText.Text = "";
+            if (LoginText.Text != "" || EmailText.Text != "" || PasswordText.Password != "" || SecondPasswordText.Password != "")
             {
+                if (LoginText.Text.Contains(" ") || EmailText.Text.Contains(" ") || PasswordText.Password.Contains(" ") || SecondPasswordText.Password.Contains(" "))
+                {
+                    MessageBox.Show("Пробіли в полях недопустимі!");
+                    return;
+                }
+                else if (PasswordText.Password == SecondPasswordText.Password)
+                {
+                    string login = LoginText.Text;
+                    string email = EmailText.Text;
 
+                    if (!IsLoginExist(login))
+                    {
+                        if (!IsEmailExist(email))
+                        {
+                            RegisterNewUser(login, email, PasswordText.Password);
+                            Login_Click(sender, e);
+                        }
+                        else
+                        {
+                            emailText.Text = "Цей email вже зареєстрований";
+                        }
+                    }
+                    else
+                    {
+                        loginText.Text = "Цей логін вже існує";
+                    }
+                }
+                else
+                {
+                    RepeatpasswordText.Text = "Вказаний пароль відрізняється від нового пароля";
+                }
             }
             else
             {
-                RepeatpasswordText.Text = "Вказаний пароль відрізняється від нового пароля";
+                MessageBox.Show("Всі поля не заповненні!");
+            }
+ 
+        }
+
+        private bool IsLoginExist(string login)
+        {
+            bool exist = false;
+            try
+            {
+                connection.Open();
+                string query = "SELECT COUNT(*) FROM users WHERE login = @Login";
+                using (NpgsqlCommand cmd = new NpgsqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("Login", login);
+                    exist = (long)cmd.ExecuteScalar() > 0;
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                Console.WriteLine("Помилка перевірки логіна: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return exist;
+        }
+
+        private bool IsEmailExist(string email)
+        {
+            bool exist = false;
+            try
+            {
+                connection.Open();
+                string query = "SELECT COUNT(*) FROM users WHERE email = @Email";
+                using (NpgsqlCommand cmd = new NpgsqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("Email", email);
+                    exist = (long)cmd.ExecuteScalar() > 0;
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                Console.WriteLine("Помилка перевірки email: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return exist;
+        }
+
+        private void RegisterNewUser(string login, string email, string password)
+        {
+            try
+            {
+                connection.Open();
+                string query = "INSERT INTO users (login, email, password, role) VALUES (@Login, @Email, @Password, @Role)";
+                using (NpgsqlCommand cmd = new NpgsqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("Login", login);
+                    cmd.Parameters.AddWithValue("Email", email);
+                    cmd.Parameters.AddWithValue("Password", password);
+                    cmd.Parameters.AddWithValue("Role", 0);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Реєстрація успішна!");
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                Console.WriteLine("Помилка реєстрації користувача: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
             }
         }
+
     }
 }
