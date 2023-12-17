1888b6393f6046b70ed16b1a517ee948bf058e4b
kachanov mykola <76394135+Klaimen921@users.noreply.github.com>
2023-12-17 23:48:02 +0200

    edited DB

--- Ukrainian-greenhouse/Ukrainian-greenhouse/DataBase/Ukrainian-greenhouse.sql
+++ Ukrainian-greenhouse/Ukrainian-greenhouse/DataBase/Ukrainian-greenhouse.sql
@@ -17,42 +17,27 @@ CREATE TABLE energy_management (
     energy_id SERIAL PRIMARY KEY,
     list_id INT,
     electricity BOOLEAN NOT NULL,
-    power_consumption REAL NOT NULL,
     energy_consumed TIMESTAMP NOT NULL,
     FOREIGN KEY (list_id) REFERENCES list(list_id)
-);CREATE TABLE greenhouse_monitoring (
-    report_id serial primary key,
+);
+CREATE TABLE greenhouse_monitoring (
+    id SERIAL PRIMARY KEY,
     list_id INT,
-
+    timestamp_report TIMESTAMP NOT NULL,
     timestamp_information TIMESTAMP NOT NULL,
     temperature_information REAL NOT NULL,
     humidity_information REAL NOT NULL,
-	
-    plant_type_information VARCHAR(255) NOT NULL,
-    watering_schedule_information JSONB NOT NULL,
-    soil_moisture_level_information REAL NOT NULL,
     irrigation_time_information TIMESTAMP NOT NULL,
     irrigation_volume_information REAL NOT NULL,
-	
-    power_consumption_information REAL NOT NULL,
-    energy_consumed_information TIMESTAMP NOT NULL,
     FOREIGN KEY (list_id) REFERENCES list(list_id)
 );
 create table list (
 	list_id SERIAL PRIMARY KEY,
 	name_of_culture VARCHAR(25)
 );
-
-INSERT INTO list VALUES (1, 'Potato'),
-						(2, 'Tomato'),
-						(3, 'Konoplya');
-
 CREATE TABLE watering_schedule (
     watering_id SERIAL PRIMARY KEY,
     list_id INT,
-    plant_type VARCHAR(255) NOT NULL,
-    watering_schedule JSONB NOT NULL,
-    soil_moisture_level REAL NOT NULL,
     irrigation_time TIMESTAMP NOT NULL,
     irrigation_volume REAL NOT NULL,
     FOREIGN KEY (list_id) REFERENCES list(list_id)
