a4db98b00ca263767788a18fb7db2c90d3c7dfec
kachanov mykola <76394135+Klaimen921@users.noreply.github.com>
2023-12-19 01:37:02 +0200

    DB

--- Ukrainian-greenhouse/Ukrainian-greenhouse/DataBase/Ukrainian-greenhouse.sql
+++ Ukrainian-greenhouse/Ukrainian-greenhouse/DataBase/Ukrainian-greenhouse.sql
@@ -18,9 +18,9 @@ create table climate_control (
 CREATE TABLE energy_management (
     energy_id SERIAL PRIMARY KEY,
     list_id INT,
-    electricity BOOLEAN NOT NULL,
     energy_consumed TIMESTAMP NOT NULL,
-    FOREIGN KEY (list_id) REFERENCES list(list_id)
+    number_of_lamps integer NOT NULL,
+    FOREIGN KEY (list_id) REFERENCES list(list_id),
 );
 CREATE TABLE greenhouse_monitoring (
     id SERIAL PRIMARY KEY,
@@ -31,6 +31,9 @@ CREATE TABLE greenhouse_monitoring (
     humidity_information REAL NOT NULL,
     irrigation_time_information TIMESTAMP NOT NULL,
     irrigation_volume_information REAL NOT NULL,
+    energy_consumed TIMESTAMP NOT NULL,
+    number_of_lamps int NOT NULL,
+    total_energy_consumed double precision NOT NULL,
     FOREIGN KEY (list_id) REFERENCES list(list_id)
 );
 CREATE TABLE watering_schedule (
@@ -40,4 +43,16 @@ CREATE TABLE watering_schedule (
     irrigation_volume REAL NOT NULL,
     FOREIGN KEY (list_id) REFERENCES list(list_id)
 );
-						
+CREATE table list_lamp (
+	lamp_id SERIAL PRIMARY KEY,
+	name_of_lamp VARCHAR (50),
+	power_use double precision
+);
+INSERT INTO list_lamp
+VALUES ( 1, 'світлодіодні', '0.0125'),
+( 2, 'люмінесцентні', '0.019'),
+( 3, 'натрієві', '0.011'),
+( 4, 'ртутні', '0.145'),
+( 5, 'металогалогенні', '0.15'),
+( 6, 'інфрачервоні', '0.175'),
+( 7, 'потужні лампи розжарювання', '0.075')
